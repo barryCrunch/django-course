91c62fa4bba959157a1713a04a08d859e29a1af3
barryCrunch <mbarry@packetdriving.com>
2023-11-04 12:47:37 -0500

    Updated blog with database models

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,6 +6,17 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}, {self.code}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -14,11 +25,16 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.street}, {self.city} {self.postal_code}"
 
+    class Meta:
+        verbose_name_plural = "Address Entires"
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True, blank=True
+    )
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -34,6 +50,9 @@ class Book(models.Model):
     )
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(
+        Country, blank=True, related_name="books"
+    )
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address = models.EmailField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return f"{self.caption}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=80)
+    excerpt = models.TextField(max_length=200)
+    content = models.TextField()
+    date = models.DateField()
+    image = models.CharField(max_length=50, null=True, blank=True)
+    slug = models.SlugField(
+        default="", unique=True, blank=True, null=False, db_index=True
+    )
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    tags = models.ManyToManyField(Tag, blank=True, related_name="tags")
+
+    def __str__(self) -> str:
+        return f"{self.title}"
