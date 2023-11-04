e14723a11f831a9c52fcb8bb8f3cb74cf38323d3
barryCrunch <mbarry@packetdriving.com>
2023-11-04 08:55:20 -0500

    book outlet

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address
 
 # Register your models here.
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("rating", "author")
+    list_display = ("title", "author")
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
